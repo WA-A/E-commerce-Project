330e971373fd4a99c1b0af8fbe6ffa2c3a697b20
wasan <wasanawwad2020@gmail.com>
2023-12-15 19:27:43 +0200

    User Profile

---
+++ src/component/profile/Profile.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../web/content/User'
+import style from './ProfileModels.css'
+
+function Profile() {
+    const {userData}=useContext(UserContext);
+    console.log(userData);
+
+  return (
+   <aside className={`${style.profile}`}> 
+    <div className='user-info'>
+        <h2>{userData.userName}</h2>
+        <img src={user.Data.image.secure_url}/>
+    </div>
+    <div className='user-contant'>
+        <h2 className='test'>{userData.email}</h2>
+        <h2>{userData.phone}</h2>
+    </div>
+   </aside>
+  )
+}
+
+export default Profile
